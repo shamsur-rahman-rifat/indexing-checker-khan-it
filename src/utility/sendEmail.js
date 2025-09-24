51fe6fc500ccd8398e0c9a9e408f85477f86f05c
shamsur-rahman-rifat <shamsur30rahman@gmail.com>
2025-09-24 22:35:21 +0600

    Webmail Added

--- src/utility/sendEmail.js
+++ src/utility/sendEmail.js
@@ -1,24 +1,31 @@
-  import { createTransport } from 'nodemailer';
+import { createTransport } from 'nodemailer';
 
 const sendEmail = async (to, text, subject) => {
     let transporter = createTransport({
-      service: 'gmail', // You can use other email providers as well
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS 
-      }
+        host: 'mail.bayas.com.bd', // Outgoing server
+        port: 465, // SMTP Port
+        secure: true, // Use SSL
+        auth: {
+            user: 'admin@bayas.com.bd', // Your email address
+            pass: 'N!Z~DTP*r61JNftH' // Your email password
+        }
     });
-  
+
     let mailOptions = {
-      from: `"Indexing Checker" <${process.env.EMAIL_USER}>`,
-      to: to,
-      subject: subject, 
-      text: text
+        from: '"Indexing Checker" <admin@bayas.com.bd>', // Your from address
+        to: to,
+        subject: subject,
+        text: text
     };
-  
-    return await transporter.sendMail(mailOptions);
-}
+
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ', info.response);
+        return info;
+    } catch (error) {
+        console.error('Error sending email: ', error);
+        throw error; // Re-throwing the error so it can be handled elsewhere
+    }
+};
 
 export default sendEmail;
-
-
